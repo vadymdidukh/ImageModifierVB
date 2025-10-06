838c23d1c2236056ad14a78cbe513c9bb5e0a101
Vadym Didukh <156947413+vadymdidukh@users.noreply.github.com>
2025-10-06 13:10:54 -0400

    Add files via upload
    Added Visual Basic project that converts color images to grayscale using multiple filters:
    Monochrome, Luma, Desaturation, Decomposition, and Single-Channel methods.

---
+++ frmImageModifier.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmImageModifier
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnOpen = New System.Windows.Forms.Button()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.cboFilter = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.picOriginal = New System.Windows.Forms.PictureBox()
+        Me.picConverted = New System.Windows.Forms.PictureBox()
+        CType(Me.picOriginal, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picConverted, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnOpen
+        '
+        Me.btnOpen.BackColor = System.Drawing.Color.Maroon
+        Me.btnOpen.FlatAppearance.BorderSize = 0
+        Me.btnOpen.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnOpen.ForeColor = System.Drawing.Color.White
+        Me.btnOpen.Location = New System.Drawing.Point(12, 11)
+        Me.btnOpen.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnOpen.Name = "btnOpen"
+        Me.btnOpen.Size = New System.Drawing.Size(120, 26)
+        Me.btnOpen.TabIndex = 0
+        Me.btnOpen.Text = "Open Image"
+        Me.btnOpen.UseVisualStyleBackColor = False
+        '
+        'btnConvert
+        '
+        Me.btnConvert.BackColor = System.Drawing.Color.LightSeaGreen
+        Me.btnConvert.FlatAppearance.BorderSize = 0
+        Me.btnConvert.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnConvert.ForeColor = System.Drawing.Color.White
+        Me.btnConvert.Location = New System.Drawing.Point(505, 12)
+        Me.btnConvert.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(120, 26)
+        Me.btnConvert.TabIndex = 1
+        Me.btnConvert.Text = "Convert To"
+        Me.btnConvert.UseVisualStyleBackColor = False
+        '
+        'btnSave
+        '
+        Me.btnSave.BackColor = System.Drawing.Color.Green
+        Me.btnSave.FlatAppearance.BorderSize = 0
+        Me.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSave.ForeColor = System.Drawing.Color.White
+        Me.btnSave.Location = New System.Drawing.Point(644, 11)
+        Me.btnSave.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(120, 26)
+        Me.btnSave.TabIndex = 2
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = False
+        '
+        'cboFilter
+        '
+        Me.cboFilter.BackColor = System.Drawing.Color.Indigo
+        Me.cboFilter.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.cboFilter.ForeColor = System.Drawing.Color.White
+        Me.cboFilter.FormattingEnabled = True
+        Me.cboFilter.Items.AddRange(New Object() {"   Monochrome", "   Gray - Averaging", "   Gray - Luma", "   Desaturation", "   Decomposition - Max", "   Decomposition - Min"})
+        Me.cboFilter.Location = New System.Drawing.Point(137, 11)
+        Me.cboFilter.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.cboFilter.Name = "cboFilter"
+        Me.cboFilter.Size = New System.Drawing.Size(361, 24)
+        Me.cboFilter.TabIndex = 3
+        Me.cboFilter.Text = "   Please select a filter"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.LightSteelBlue
+        Me.Label1.Location = New System.Drawing.Point(13, 85)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 16)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Original"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.LightSteelBlue
+        Me.Label2.Location = New System.Drawing.Point(591, 84)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 16)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Preview"
+        '
+        'picOriginal
+        '
+        Me.picOriginal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.picOriginal.Location = New System.Drawing.Point(17, 105)
+        Me.picOriginal.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.picOriginal.Name = "picOriginal"
+        Me.picOriginal.Size = New System.Drawing.Size(557, 428)
+        Me.picOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picOriginal.TabIndex = 6
+        Me.picOriginal.TabStop = False
+        '
+        'picConverted
+        '
+        Me.picConverted.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.picConverted.Location = New System.Drawing.Point(595, 105)
+        Me.picConverted.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.picConverted.Name = "picConverted"
+        Me.picConverted.Size = New System.Drawing.Size(557, 428)
+        Me.picConverted.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picConverted.TabIndex = 7
+        Me.picConverted.TabStop = False
+        '
+        'frmImageModifier
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(1167, 587)
+        Me.Controls.Add(Me.picConverted)
+        Me.Controls.Add(Me.picOriginal)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cboFilter)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.btnOpen)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Name = "frmImageModifier"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Image Modifier"
+        CType(Me.picOriginal, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picConverted, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnOpen As Button
+    Friend WithEvents btnConvert As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents cboFilter As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents picOriginal As PictureBox
+    Friend WithEvents picConverted As PictureBox
+End Class
---
+++ frmImageModifier.vb
@@ -0,0 +1,201 @@
+﻿Public Class frmImageModifier
+    Dim myImage As Bitmap
+
+    Private Sub btnOpen_Click(sender As Object, e As EventArgs) Handles btnOpen.Click
+        Dim open As New OpenFileDialog
+        open.Title = "Image Location"
+        open.Filter = "JPeg Image|*.jpg|All files (*.*)|*.*"
+
+        If open.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            myImage = New Bitmap(open.FileName, True)
+            picOriginal.BorderStyle = BorderStyle.None
+            picOriginal.Image = myImage
+            picConverted.Image = Nothing
+        End If
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        If picConverted.Image IsNot Nothing Then
+            Dim save As New SaveFileDialog
+            save.Title = "Save Directory"
+            save.Filter = "JPeg Image|*.jpg|All files (*.*)|*.*"
+
+            If save.ShowDialog() = Windows.Forms.DialogResult.OK Then
+                picConverted.Image.Save(save.FileName)
+                MessageBox.Show("The converted image has been successfully saved!")
+            End If
+        Else MessageBox.Show("There is no image to be saved.", "Error")
+        End If
+    End Sub
+
+    Private Sub UseMonochrome(imgToProcess As Bitmap)
+        Dim r, g, b, average As Integer
+        Dim y, x As Integer
+        Dim newColor As Color
+
+        For y = 0 To imgToProcess.Height - 1
+            For x = 0 To imgToProcess.Width - 1
+                Dim pixelColor As Color = imgToProcess.GetPixel(x, y)
+                r = CInt(pixelColor.R)
+                g = CInt(pixelColor.G)
+                b = CInt(pixelColor.B)
+                average = CInt((r + g + b) / 3)
+
+                If average > 128 Then
+                    newColor = Color.FromArgb(255, 255, 255)
+                Else newColor = Color.FromArgb(0, 0, 0)
+                End If
+
+                imgToProcess.SetPixel(x, y, newColor)
+            Next
+        Next
+    End Sub
+
+    Private Sub UseGrayAveraging(imgToProcess As Bitmap)
+        Dim r, g, b As Integer
+        Dim y, x As Integer
+        Dim newColor As Color
+        Dim grey As Integer
+
+        For y = 0 To imgToProcess.Height - 1
+            For x = 0 To imgToProcess.Width - 1
+                Dim pixelColor As Color = imgToProcess.GetPixel(x, y)
+                r = CInt(pixelColor.R)
+                g = CInt(pixelColor.G)
+                b = CInt(pixelColor.B)
+
+                grey = CInt((r + (g + b)) / 3)
+
+                newColor = Color.FromArgb(grey, grey, grey)
+                imgToProcess.SetPixel(x, y, newColor)
+            Next
+        Next
+    End Sub
+
+    Private Sub UseGrayLuma(imgToProcess As Bitmap)
+        Dim r, g, b As Integer
+        Dim y, x As Integer
+        Dim newColor As Color
+        Dim grey As Integer
+
+        For y = 0 To imgToProcess.Height - 1
+            For x = 0 To imgToProcess.Width - 1
+                Dim pixelColor As Color = imgToProcess.GetPixel(x, y)
+                r = CInt(pixelColor.R)
+                g = CInt(pixelColor.G)
+                b = CInt(pixelColor.B)
+
+                grey = CInt(r * 0.2126 + g * 0.7152 + b * 0.0722)
+
+                newColor = Color.FromArgb(grey, grey, grey)
+                imgToProcess.SetPixel(x, y, newColor)
+            Next
+        Next
+    End Sub
+
+    Private Sub UseDesaturation(imgToProcess As Bitmap)
+        Dim r, g, b, gray As Integer
+        Dim y, x As Integer
+        Dim newColor As Color
+
+        For y = 0 To imgToProcess.Height - 1
+            For x = 0 To imgToProcess.Width - 1
+                Dim pixelColor As Color = imgToProcess.GetPixel(x, y)
+                r = CInt(pixelColor.R)
+                g = CInt(pixelColor.G)
+                b = CInt(pixelColor.B)
+
+                gray = CInt((Math.Max(r, Math.Max(g, b)) + Math.Min(r, Math.Min(g, b))) / 2)
+
+                newColor = Color.FromArgb(gray, gray, gray)
+                imgToProcess.SetPixel(x, y, newColor)
+            Next
+        Next
+    End Sub
+
+    Private Sub UseDecomposition(imgToProcess As Bitmap, useMax As Boolean)
+        Dim r, g, b, gray As Integer
+        Dim y, x As Integer
+        Dim newColor As Color
+
+        For y = 0 To imgToProcess.Height - 1
+            For x = 0 To imgToProcess.Width - 1
+                Dim pixelColor As Color = imgToProcess.GetPixel(x, y)
+                r = CInt(pixelColor.R)
+                g = CInt(pixelColor.G)
+                b = CInt(pixelColor.B)
+
+                If useMax Then
+                    gray = Math.Max(r, Math.Max(g, b))
+                Else
+                    gray = Math.Min(r, Math.Min(g, b))
+                End If
+
+                newColor = Color.FromArgb(gray, gray, gray)
+                imgToProcess.SetPixel(x, y, newColor)
+            Next
+        Next
+    End Sub
+
+    Private Sub UseSingleChannel(imgToProcess As Bitmap, channel As Integer)
+        Dim r, g, b, gray As Integer
+        Dim y, x As Integer
+        Dim newColor As Color
+
+        For y = 0 To imgToProcess.Height - 1
+            For x = 0 To imgToProcess.Width - 1
+                Dim pixelColor As Color = imgToProcess.GetPixel(x, y)
+                r = CInt(pixelColor.R)
+                g = CInt(pixelColor.G)
+                b = CInt(pixelColor.B)
+
+                Select Case channel
+                    Case 0
+                        gray = r
+                    Case 1
+                        gray = g
+                    Case 2
+                        gray = b
+                End Select
+
+                newColor = Color.FromArgb(gray, gray, gray)
+                imgToProcess.SetPixel(x, y, newColor)
+            Next
+        Next
+    End Sub
+
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        If picOriginal.Image IsNot Nothing Then
+            If cboFilter.SelectedItem IsNot Nothing Then
+                myImage = New Bitmap(picOriginal.Image)
+                Select Case cboFilter.SelectedIndex
+                    Case 0
+                        UseMonochrome(myImage)
+                    Case 1
+                        UseGrayAveraging(myImage)
+                    Case 2
+                        UseGrayLuma(myImage)
+                    Case 3
+                        UseDesaturation(myImage)
+                    Case 4
+                        UseDecomposition(myImage, True)
+                    Case 5
+                        UseDecomposition(myImage, False)
+                    Case 6
+                        UseSingleChannel(myImage, 0)
+                    Case 7
+                        UseSingleChannel(myImage, 1)
+                    Case 8
+                        UseSingleChannel(myImage, 2)
+                End Select
+
+                picConverted.Image = myImage
+                picConverted.BorderStyle = BorderStyle.None
+            Else
+                MessageBox.Show("Please select a filter to work with.", "Error")
+            End If
+        Else
+            MessageBox.Show("Please select a picture to convert.", "Error")
+        End If
+    End Sub
+End Class
